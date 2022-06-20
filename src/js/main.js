f3fba2ce11154f04fa811affa02f7fc355dd22b9
Nikolay Tarasenko <kolyatarasenko@ukr.net>
2022-06-20 15:52:07 +0300

    add blog section

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,26 @@
 import $ from 'jquery';
 import 'slick-carousel';
 
-$('.carousel').slick({
-  infinite: false,
-  slidesToShow: 4,
-  slidesToScroll: 1,
-  prevArrow: '.team .arrow_left',
-  nextArrow: '.team .arrow_right',
+const sliders = [
+  {
+    sliderNode: '.team__carousel',
+    slidesToShow: 4,
+    prevArrowNode: '.team .arrow_left',
+    nextArrowNode: '.team .arrow_right'
+  },
+  {
+    sliderNode: '.blog__carousel',
+    slidesToShow: 3,
+    prevArrowNode: '.blog .arrow_left',
+    nextArrowNode: '.blog .arrow_right'
+  }
+];
+
+$.each(sliders, function(i, element) {
+  $(element.sliderNode).slick({
+    infinite: false,
+    slidesToShow: element.slidesToShow,
+    prevArrow: element.prevArrowNode,
+    nextArrow: element.nextArrowNode,
+  })
 });
